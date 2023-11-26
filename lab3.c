4bd787451c7127ac230382b0947e0c8a95d41d95
Sameer <sameer.patelde@gmail.com>
2023-11-26 17:55:14 +0000

    revert back to original code where there was no core dump

--- lab3.c
+++ lab3.c
@@ -13,15 +13,12 @@ int** read_board_from_file(char* filename){
 
     for(int i = 0; i < COL_SIZE; i++)
         board[i] = (int*) malloc(COL_SIZE * sizeof(int));
-
     if(fp == NULL) return;
-
 
     for(int r = 0; r < ROW_SIZE; r++)
         for(int c = 0; c < COL_SIZE; c++)
             fscanf(fp,"%d",&board[r][c]);
     fclose(fp);
-
     return board;
 }
 
@@ -33,31 +30,15 @@ int is_board_valid(){
 
     tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
     parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
-    for(int i = 0; i < ROW_SIZE; i++){
-        parameter[i].starting_row = i;
-        parameter[i].starting_col = 0;
-        parameter[i].ending_col = 0;
-        parameter[i].ending_row = i;
-        pthread_create(&(tid[i]), &attr, valid_row(sudoku_board,parameter[i]), &(parameter[i]));
-    }
+    parameter[0].starting_row = 0;
+    parameter[0].starting_col = 0;
+    parameter[0].ending_col = COL_SIZE - 1;
+    parameter[0].ending_row = 0;
 
     
     // replace this comment with your code
-    for(int i = 0;i<NUM_OF_THREADS; i++) 
-        if (worker_validation[i] != 1) return 0;
-    return 1;
+
     
 }
 
-int valid_row(int** board, param_struct p){
-    int ans[9];
-    for(int i = p.starting_row; i < p.ending_row; i++)
-        for(int z = p.starting_col; z < p.ending_row; z++){
-            int current = board[i][z];
-            ans[current - 1] = current - (current - 1);
-        }
 
-    for(int i = 0; i < ROW_SIZE; i++)
-        if(ans[i] != 1) return 0;
-    return 1;
-}
