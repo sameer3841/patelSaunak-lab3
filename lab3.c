675a108954a8d04787702b4af65ae56b14c6f081
Sameer <sameer.patelde@gmail.com>
2023-11-26 23:21:23 +0000

    creating the new method for checking each sequence, but recieving core dump errors

--- lab3.c
+++ lab3.c
@@ -22,6 +22,36 @@ int** read_board_from_file(char* filename){
     return board;
 }
 
+void* is_valid(void* arg){
+    param_struct* param = (param_struct*) arg;
+    int startrow = param->starting_row;
+    int endrow = param->ending_row;
+    int startcol = param->starting_col;
+    int endcol = param->ending_col;
+    int arr[9] = {0};
+    int current;
+
+    //checking by row
+    if(startrow == endrow){
+        for(int i = 0; i < COL_SIZE; i++){
+            current = sudoku_board[i][startcol];
+            if((current > 0 && current < 10) | arr[current - 1] == 1) pthread_exit(NULL);
+            else arr[current - 1] = 1;
+        }
+        worker_validation[startcol + 18] = 1;
+    }
+
+    else if(startcol == endcol){
+        for(int i = 0; i < COL_SIZE; i++){
+            current = sudoku_board[startrow][i];
+            if((current > 0 && current < 10) | arr[current - 1] == 1) pthread_exit(NULL);
+            else arr[current - 1] = 1;
+        }
+        worker_validation[startrow + 9] = 1;
+    }
+    pthread_exit(NULL);
+}
+
 
 int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
@@ -29,18 +59,34 @@ int is_board_valid(){
     param_struct* parameter;
 
     tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
+    int index = 0;
     worker_validation = (int*) malloc(sizeof(int) * NUM_OF_THREADS);
 
+    for(int r = 0; r < ROW_SIZE; r++){
+        for(int c = 0; c < COL_SIZE; c++){
+            if(c == 0){
+                parameter = (param_struct*) malloc(sizeof(param_struct));
+                parameter->starting_row = r;
+                parameter->starting_col = c;
+                parameter->ending_row = c;
+                parameter->ending_col = r + 1;
+                pthread_create(&tid[index++], NULL, is_valid, parameter);
+            }
+        }
 
 
-}
+    }
 
-void is_valid(param_struct* param){
-    int startrow = param->starting_row;
-    int endrow = param->ending_row;
-    int startcol = param->starting_col;
-    int endcol = param->ending_col;
-
+    for(int i = 0; i< NUM_OF_THREADS; i++)
+        pthread_join(tid[i], NULL);
     
+    for (int i = 0; i<NUM_OF_THREADS; i++)
+        if(worker_validation[i] == 0)return 0;
+    free(worker_validation);
+    free(tid);
+    return 1;
+
 }
+
+
 
