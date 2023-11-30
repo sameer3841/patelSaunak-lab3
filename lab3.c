a99108dbb0133f05b4939ac3438907a400bf7180
Sameer <sameer.patelde@gmail.com>
2023-11-30 13:54:19 +0000

    Invalids are showing but don't know why is_board_valid is always returning false

--- lab3.c
+++ lab3.c
@@ -7,28 +7,72 @@ extern int** sudoku_board;
 int* worker_validation;
 
 int** read_board_from_file(char* filename){
-
     FILE *fp = NULL;
-    sudoku_board = (int**)malloc(ROW_SIZE * sizeof(int*));
+    sudoku_board = (int**)malloc(sizeof(int*)*ROW_SIZE);
+    for(int row = 0; row < ROW_SIZE; row++){
+	    sudoku_board[row] = (int*)malloc(sizeof(int)*COL_SIZE);
+    }
 
-    for(int i = 0; i < COL_SIZE; i++)
-        sudoku_board[i] = (int*) malloc(COL_SIZE * sizeof(int));
-    
     fp = fopen(filename,"r");
-    if(fp == NULL) return NULL;
-    for(int r = 0; r < ROW_SIZE; r++)
-        for(int c = 0; c < COL_SIZE; c++)
-            fscanf(fp,"%d",&sudoku_board[r][c]);
+    for(int i = 0; i < ROW_SIZE; i++) {
+        for(int j = 0; j < COL_SIZE; j++) {
+            fscanf(fp, "%d%*c", &sudoku_board[i][j]);
+        }
+    }   
 
-    for(int x = 0; x < ROW_SIZE; x++){
-        for(int y = 0; y < COL_SIZE; y++) printf("%d ",sudoku_board[x][y]);
+    for(int i = 0; i<ROW_SIZE; i++){
+        for(int j = 0; j<COL_SIZE; j++)
+        {
+            printf("%d ", sudoku_board[i][j]);
+        }
         printf("\n");
     }
+
     fclose(fp);
     return sudoku_board;
 }
 
-void* is_valid(void* arg){
+void* validateRow(void* arg){
+    param_struct* param = (param_struct*) arg;
+    int startrow = param->starting_row;
+    int endrow = param->ending_row;
+    int startcol = param->starting_col;
+    int endcol = param->ending_col;
+    int current;
+    int arr[9] = {0};
+
+    for(int i = 0; i < ROW_SIZE; i++){
+            current = sudoku_board[startrow][i];
+            if((current > 9 || current < 1) || arr[current - 1] == 1) {
+                printf("Row: %d and Column: %d\n",startrow+1,i+1);
+                pthread_exit(NULL);
+            }
+            else arr[current - 1] = 1;
+        }
+        worker_validation[startrow + 9] = 1;
+}
+
+void* validateCol(void* arg){
+    param_struct* param = (param_struct*) arg;
+    int startrow = param->starting_row;
+    int startcol = param->starting_col;
+    int current;
+    int arr[9] = {0};
+
+
+    for(int i = 0; i < ROW_SIZE; i++){
+            current = sudoku_board[startrow][i];
+            if((current > 9 || current < 1) || arr[current - 1] == 1){ 
+                printf("Row: %d and Column: %d\n",i+1,startcol+1);
+                pthread_exit(NULL);
+            }
+            else arr[current - 1] = 1;
+        }
+        worker_validation[startrow + 9] = 1;
+        pthread_exit(NULL);
+}
+
+void* validate3x3(void* arg){
     param_struct* param = (param_struct*) arg;
     int startrow = param->starting_row;
     int endrow = param->ending_row;
@@ -37,32 +81,18 @@ void* is_valid(void* arg){
     int arr[9] = {0};
     int current;
 
-    if(startrow == endrow){
-        for(int i = 0; i < COL_SIZE; i++){
-            current = sudoku_board[i][startcol];
-            if((current > 9 || current < 1) || arr[current - 1] == 1) pthread_exit(NULL);
-            else arr[current - 1] = 1;
-        }
-        worker_validation[startcol + 18] = 1;
-    }
-
-    else if(startcol == endcol){
-        for(int i = 0; i < ROW_SIZE; i++){
-            current = sudoku_board[startrow][i];
-            if((current > 9 || current < 1) || arr[current - 1] == 1) pthread_exit(NULL);
-            else arr[current - 1] = 1;
-        }
-        worker_validation[startrow + 9] = 1;
-    } else {
-        for(int r = startrow; r < startrow + 3; r++){
-            for(int c = startcol; c < startcol + 3; c++){
-                current = sudoku_board[r][c];
-                if((current > 9 || current < 1) || arr[current - 1] == 1) pthread_exit(NULL);
-                else arr[current - 1] = 1;
+    
+    for(int r = startrow; r < startrow + 3; r++){
+        for(int c = startcol; c < startcol + 3; c++){
+            current = sudoku_board[r][c];
+            if((current > 9 || current < 1) || arr[current - 1] == 1) {
+                printf("Row: %d and Column: %d\n",r+1,c+1);
+                pthread_exit(NULL);
             }
+            else arr[current - 1] = 1;
         }
-        worker_validation[startrow + startcol / 3] = 1;
     }
+    worker_validation[startrow + startcol / 3] = 1;
     pthread_exit(NULL);
 }
 
@@ -75,7 +105,7 @@ int is_board_valid(){
     tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
     int index = 0;
     worker_validation = (int*) malloc(sizeof(int) * NUM_OF_THREADS);
-
+    printf("Invalids:\n");
     for(int r = 0; r < ROW_SIZE; r++){
         for(int c = 0; c < COL_SIZE; c++){
             if(r % 3 == 0 && c % 3 == 0){
@@ -84,7 +114,7 @@ int is_board_valid(){
                 parameter->starting_col = c;
                 parameter->ending_row = r+1;
                 parameter->ending_col = c+1;
-                pthread_create(&tid[index++], NULL, is_valid, parameter);
+                pthread_create(&tid[index++], NULL, validate3x3, parameter);
             }
             if(r == 0){
                 parameter = (param_struct*) malloc(sizeof(param_struct*));
@@ -92,7 +122,7 @@ int is_board_valid(){
                 parameter->starting_col = c;
                 parameter->ending_row = r;
                 parameter->ending_col = c+1;
-                pthread_create(&tid[index++], NULL, is_valid, parameter);
+                pthread_create(&tid[index++], NULL, validateCol, parameter);
             }
             if(c == 0){
                 parameter = (param_struct*) malloc(sizeof(param_struct*));
@@ -100,18 +130,19 @@ int is_board_valid(){
                 parameter->starting_col = c;
                 parameter->ending_row = c;
                 parameter->ending_col = r+1;
-                pthread_create(&tid[index++], NULL, is_valid, parameter);
+                pthread_create(&tid[index++], NULL, validateRow, parameter);
             }
         }
 
 
     }
 
-    for(int i = 0; i< NUM_OF_THREADS; i++)
+    for(int i = 0; i< NUM_OF_THREADS; i++){
         pthread_join(tid[i], NULL);
-    
-    for (int i = 0; i<NUM_OF_THREADS; i++)
+    }
+    for (int i = 0; i<NUM_OF_THREADS; i++){
         if(worker_validation[i] == 0)return 0;
+    }
     free(worker_validation);
     free(tid);
     return 1;
