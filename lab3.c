83d98d3717b9d41d0a82f2c159a55964437a2d10
Sameer <sameer.patelde@gmail.com>
2023-12-01 00:28:44 +0000

    completed

--- lab3.c
+++ lab3.c
@@ -4,7 +4,8 @@
 #include "lab3.h"
 
 extern int** sudoku_board;
-int* worker_validation;
+int* validation;
+
 
 int** read_board_from_file(char* filename){
     FILE *fp = NULL;
@@ -32,118 +33,94 @@ int** read_board_from_file(char* filename){
     return sudoku_board;
 }
 
-void* validateRow(void* arg){
-    param_struct* param = (param_struct*) arg;
-    int startrow = param->starting_row;
-    int endrow = param->ending_row;
-    int startcol = param->starting_col;
-    int endcol = param->ending_col;
-    int current;
-    int arr[9] = {0};
 
-    for(int i = 0; i < ROW_SIZE; i++){
-            current = sudoku_board[startrow][i];
-            if((current > 9 || current < 1) || arr[current - 1] == 1) {
-                printf("Row: %d and Column: %d\n",startrow+1,i+1);
-                pthread_exit(NULL);
-            }
-            else arr[current - 1] = 1;
-        }
-        worker_validation[startrow + 9] = 1;
-}
-
-void* validateCol(void* arg){
-    param_struct* param = (param_struct*) arg;
-    int startrow = param->starting_row;
-    int startcol = param->starting_col;
-    int current;
-    int arr[9] = {0};
-
-
-    for(int i = 0; i < ROW_SIZE; i++){
-            current = sudoku_board[startrow][i];
-            if((current > 9 || current < 1) || arr[current - 1] == 1){ 
-                printf("Row: %d and Column: %d\n",i+1,startcol+1);
-                pthread_exit(NULL);
-            }
-            else arr[current - 1] = 1;
-        }
-        worker_validation[startrow + 9] = 1;
-        pthread_exit(NULL);
-}
-
-void* validate3x3(void* arg){
-    param_struct* param = (param_struct*) arg;
-    int startrow = param->starting_row;
-    int endrow = param->ending_row;
-    int startcol = param->starting_col;
-    int endcol = param->ending_col;
-    int arr[9] = {0};
-    int current;
-
+void* validate(void* arg){
     
-    for(int r = startrow; r < startrow + 3; r++){
-        for(int c = startcol; c < startcol + 3; c++){
+    param_struct* param = (param_struct*) arg;
+    int id = param->id;
+    int startrow = param->starting_row;
+    int endrow = param->ending_row;
+    int startcol = param->starting_col;
+    int endcol = param->ending_col;
+    int arr[9] = {0,0,0,0,0,0,0,0,0};
+    int current;
+
+    validation[id] = 1;
+    
+    for(int r = startrow; r <= endrow; r++){
+        for(int c = startcol; c <= endcol; c++){
             current = sudoku_board[r][c];
-            if((current > 9 || current < 1) || arr[current - 1] == 1) {
-                printf("Row: %d and Column: %d\n",r+1,c+1);
-                pthread_exit(NULL);
-            }
-            else arr[current - 1] = 1;
+            arr[current - 1] = 1;
         }
     }
-    worker_validation[startrow + startcol / 3] = 1;
-    pthread_exit(NULL);
+    
+    for(int i = 0; i < 9; i++)
+        if(arr[i] != 1) validation[id] = 0;
+    
+    
 }
 
 
 int is_board_valid(){
     pthread_t* tid;  /* the thread identifiers */
     pthread_attr_t attr;
-    param_struct* parameter;
-
-    tid = (pthread_t*) malloc(sizeof(pthread_t) * NUM_OF_THREADS);
+    
+    tid = (pthread_t*) malloc(sizeof(int*) * NUM_OF_THREADS);
+    param_struct* parameter = (param_struct*) malloc(sizeof(param_struct) * NUM_OF_THREADS);
     int index = 0;
-    worker_validation = (int*) malloc(sizeof(int) * NUM_OF_THREADS);
-    printf("Invalids:\n");
-    for(int r = 0; r < ROW_SIZE; r++){
-        for(int c = 0; c < COL_SIZE; c++){
-            if(r % 3 == 0 && c % 3 == 0){
-                parameter = (param_struct*) malloc(sizeof(param_struct));
-                parameter->starting_row = r;
-                parameter->starting_col = c;
-                parameter->ending_row = r+1;
-                parameter->ending_col = c+1;
-                pthread_create(&tid[index++], NULL, validate3x3, parameter);
-            }
-            if(r == 0){
-                parameter = (param_struct*) malloc(sizeof(param_struct));
-                parameter->starting_row = r;
-                parameter->starting_col = c;
-                parameter->ending_row = r;
-                parameter->ending_col = c+1;
-                pthread_create(&tid[index++], NULL, validateCol, parameter);
-            }
-            if(c == 0){
-                parameter = (param_struct*) malloc(sizeof(param_struct));
-                parameter->starting_row = r;
-                parameter->starting_col = c;
-                parameter->ending_row = c;
-                parameter->ending_col = r+1;
-                pthread_create(&tid[index++], NULL, validateRow, parameter);
-            }
-        }
+    validation = (int*) malloc(sizeof(int) * 27);
+    
+    for(int i = 0; i < ROW_SIZE; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = i;
+        parameter[index].starting_col = 0;
+        parameter[index].ending_row = i;
+        parameter[index].ending_col = COL_SIZE - 1;
 
-
+        pthread_create(&(tid[index]), NULL, validate, &parameter[index]);
+        index++;
     }
 
-    for(int i = 0; i< NUM_OF_THREADS; i++){
+    for(int i = 0; i < COL_SIZE; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = 0;
+        parameter[index].starting_col = i;
+        parameter[index].ending_row = COL_SIZE - 1;
+        parameter[index].ending_col = i;
+
+        pthread_create(&(tid[index]), NULL, validate, &parameter[index]);
+        index++;
+    }
+
+    int r = 0, c = 0;
+    for(int i = 0; i < NUM_OF_SUBGRIDS; i++){
+        parameter[index].id = index;
+        parameter[index].starting_row = r;
+        parameter[index].starting_col = c;
+        parameter[index].ending_row = r + 2;
+        parameter[index].ending_col = c + 2;
+
+        pthread_create(&(tid[index]), NULL, validate, &parameter[index]);
+        index++;
+        if(c == 6){
+            r += 3;
+            c = 0;
+        } else c += 3;
+    }
+
+    
+    for(int i = 0; i< 27; i++){
+        
         pthread_join(tid[i], NULL);
     }
-    for (int i = 0; i<NUM_OF_THREADS; i++){
-        if(worker_validation[i] == 0)return 0;
+
+    for (int x = 0; x<27; x++){
+        if(validation[x] != 1){
+            return 0;
+        }
     }
-    free(worker_validation);
+
+    free(validation);
     free(tid);
     return 1;
 }
