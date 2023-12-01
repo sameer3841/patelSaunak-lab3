818f2f13e0bbd624e1278ef080da0a7b1ef9ec42
Sameer <sameer.patelde@gmail.com>
2023-12-01 00:36:15 +0000

    done and modified

--- lab3.c
+++ lab3.c
@@ -10,22 +10,18 @@ int* validation;
 int** read_board_from_file(char* filename){
     FILE *fp = NULL;
     sudoku_board = (int**)malloc(sizeof(int*)*ROW_SIZE);
-    for(int row = 0; row < ROW_SIZE; row++){
+    for(int row = 0; row < ROW_SIZE; row++)
 	    sudoku_board[row] = (int*)malloc(sizeof(int)*COL_SIZE);
-    }
+    
 
     fp = fopen(filename,"r");
-    for(int i = 0; i < ROW_SIZE; i++) {
-        for(int j = 0; j < COL_SIZE; j++) {
+    for(int i = 0; i < ROW_SIZE; i++)
+        for(int j = 0; j < COL_SIZE; j++) 
             fscanf(fp, "%d%*c", &sudoku_board[i][j]);
-        }
-    }   
+
 
     for(int i = 0; i<ROW_SIZE; i++){
-        for(int j = 0; j<COL_SIZE; j++)
-        {
-            printf("%d ", sudoku_board[i][j]);
-        }
+        for(int j = 0; j<COL_SIZE; j++) printf("%d ", sudoku_board[i][j]);
         printf("\n");
     }
 
